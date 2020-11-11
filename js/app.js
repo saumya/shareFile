67d5be7e087dca8c43ebef1fb3d1071a983b5255
saumya <2saumyaray@gmail.com>
2020-11-11 13:27:55 +0530

    implementing the upload

--- js/app.js
+++ js/app.js
@@ -5,7 +5,24 @@ console.log('trying');
 console.groupEnd();
 //
 $(function(){
-    console.log('Ready!')
+    console.log('Ready!');
+    console.log( $('#in_File')[0] );
+    console.log( $('#btn_upload')[0] );
+    
+    $('#in_File').on('change', function(event){
+        console.log( 'change', new Date() );
+        console.log(event.target);
+        
+        const name = event.target.files[0].name;
+        const lastDot = name.lastIndexOf('.');
+        const fileName = name.substring(0, lastDot);
+        const ext = name.substring(lastDot + 1);
+
+        console.log(fileName);
+        console.log(ext);
+
+        return false;
+    });
 });
 
 
