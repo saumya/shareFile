4b2b6d53194fdcad811d5517ffb2a1ca2b2e706e
saumya <2saumyaray@gmail.com>
2020-11-11 14:22:25 +0530

    implementing the image upload

--- js/app.js
+++ js/app.js
@@ -6,13 +6,14 @@ console.groupEnd();
 //
 $(function(){
     console.log('Ready!');
-    console.log( $('#in_File')[0] );
-    console.log( $('#btn_upload')[0] );
+    //console.log( $('#in_File')[0] );
+    //console.log( $('#btn_upload')[0] );
     
     $('#in_File').on('change', function(event){
-        console.log( 'change', new Date() );
-        console.log(event.target);
+        //console.log( 'change', new Date() );
+        //console.log(event.target);
         
+        /*
         const name = event.target.files[0].name;
         const lastDot = name.lastIndexOf('.');
         const fileName = name.substring(0, lastDot);
@@ -20,15 +21,31 @@ $(function(){
 
         console.log('name', fileName);
         console.log('extension', ext);
+        */
         //
         //const reader = new FileReader();
         //console.log( 'reader', reader );
 
         const file = event.target.files[0];
+        /*
         console.log( 'file', file );
         console.log( 'name', file.name );
         console.log( 'size', file.size );
         console.log( 'type', file.type );
+        */
+        const reader = new FileReader();
+        reader.addEventListener('load', (event) => {
+            //img.src = event.target.result;
+            $('#img_file')[0].src = event.target.result;
+            //$('#img_file').src = event.target.result;
+        });
+        reader.addEventListener('progress', (event) => {
+            if (event.loaded && event.total) {
+              const percent = (event.loaded / event.total) * 100;
+              console.log(`Progress: ${Math.round(percent)}`);
+            }
+        });
+        reader.readAsDataURL(file);
 
         //
         return false;
